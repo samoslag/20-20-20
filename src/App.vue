3f0690cd914be51191ba2193d1efca9a7121db67
Samo Slag <samo.slag@gmail.com>
2024-03-13 22:41:59 +0100

    Deploy

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <router-view/>
-</template>
-
-<style lang="scss">
-@import '@/styles/index.scss';
-</style>
--- src/helpers/time.ts
+++
@@ -1,1 +0,0 @@
-export const format = (value = 0) => value < 10 ? `0${value}` : String(value)
